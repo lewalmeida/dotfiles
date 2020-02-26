4457fea646454487e50f09c71d0b1232c574a24b
Leonardo de Almeida <lewalmeida@icloud.com>
2020-02-26 16:23:21 -0300

    files

---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+echo 'Starting...';
+
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || {
+    echo >&2 "Installing Homebrew"; \
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; 
+}
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install zsh
+brew install zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Note: don’t forget to add `/usr/local/bin/zsh` to `/etc/shells` before
+# running `chsh`.
+chsh -s $(which zsh)
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install openssh
+
+# Install other useful binaries.
+brew install git
+brew install git-extras
+brew install imagemagick --with-webp
+brew install p7zip
+brew install htop
+
+# Install brew cask and other apps
+# Hit Ctrl+C to stop anytime
+brew install brew-cask
+brew cask install iterm2
+brew cask install caffeine
+brew cask install imageoptim
+brew cask install github
+brew cask install sequel-pro
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install kap
+
+# Install apps from App Store
+mas lucky Bear
+
+# Add sublime command
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+
+# Install PHP, MySQL, redis and beanstalkd
+brew install mysql@5.7 && brew link mysql@5.7 --force
+brew install php72
+
+cp ./../config/php-memory-limits.ini /usr/local/etc/php/7.2/conf.d/php-memory-limits.ini
+
+brew install node@10 && brew postinstall node@10
+
+# Remove outdated versions from the cellar.
+brew cleanup & brew prune
+
+# List services to check them
+brew services list
